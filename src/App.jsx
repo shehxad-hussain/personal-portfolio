66137a415119c02c5e39f25bd0baee6596e265c7
m-shehzad455 <shehzuali5@gmail.com>
2023-06-14 05:36:41 +0500

    update experties section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import css from "./styles/app.module.scss"
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-
+import Experties from "./components/Experties/Experties";
 
 
 const App = () => {
    return <div className={`bg-primary ${css.container}`}>
       <Header />
       <Hero />
+      <Experties />
    </div>
 };
 
